b3420667d65f88d9c6676806363d4f51b798d2c0
David Sabalete <dsabalete@gmail.com>
2023-07-03 23:07:46 +0200

    feat: SiteHeader and home page

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
-    <h1>Hello, Nuxt 3!</h1>
+    <SiteHeader />
+    <main class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <NuxtPage />
+    </main>
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+</script>
+
+<template>
+    <section>
+        <h1 class="text-5xl font-bold mt-20">👋 Hello, I'm David Sabalete!</h1>
+        <p class="text-base text-gray-900 p-2 italic">FE Developer at LoveToKnow Media</p>
+    </section>
+
+    <section class="flex flex-col md:flex">
+        <div class="md:w-3/4">
+
+            <p class="text-lg py-2">I am experienced in Vue.js and Nuxt.js in which I produce high quality code for my
+                clients that are both well
+                performing and well tested.</p>
+            <p class="text-lg py-2">I focus on solving problems, not only technical problems but creating an awesome
+                experience for the users of my
+                software both visual and accessible. Mixing powerful tools and knowledge about the users brings visual UI's
+                to
+                life and leads to webpages being accessible and useful.</p>
+            <p class="text-lg py-2">I am a team player and I enjoy working in a team where we can learn from each other and
+                grow together. I am also
+                a fast learner and I am always looking for new challenges and opportunities to grow as a developer.</p>
+    </div>
+</section></template>
